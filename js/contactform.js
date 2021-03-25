1f5254c4ecc169ffcfd1c4debe6e7cd1bdadbc64
timhoward681 <tim.j.howard@gmail.com>
2021-03-25 19:40:22 -0400

    First Commit

---
+++ js/contactform.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('.submit').click (function (event) {
+     console.log('clicked button')
+   
+  var yourName = $('.yourName').val()
+  var email = $('.emailadd').val()
+  var newsletter = $('.newsletter').val()  
+  
+
+  if(email.includes('@') && email.includes('.')){
+      console.log('<div>Email is valid</div>')
+      } else { 
+          event.preventDefault ()
+          console.log('<div>Email is not valid</div>')
+      }
+
+     
+   })
+})
+
